05bf8fa6dde065e42caeebb2176585d0bb6a33f4
Jakkarin-ko <jakkarin2534@gmail.com>
2025-10-28 05:53:46 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App.tsx' // หรือ Component หลักเดิมของคุณ
 import ServerStatusManager from './src/ServerStatusManager.tsx'; // ⭐️ นำเข้า Component ใหม่ ⭐️
-import './src/index.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
