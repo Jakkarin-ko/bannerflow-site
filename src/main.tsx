2eb84ab170524a2718ece6b35a5bf3e8da2a553e
Jakkarin-ko <jakkarin2534@gmail.com>
2025-10-28 05:54:47 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App.tsx' // หรือ Component หลักเดิมของคุณ
-import ServerStatusManager from './src/ServerStatusManager.tsx'; // ⭐️ นำเข้า Component ใหม่ ⭐️
+import ServerStatusManager from './ServerStatusManager.tsx'; // ⭐️ นำเข้า Component ใหม่ ⭐️
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
