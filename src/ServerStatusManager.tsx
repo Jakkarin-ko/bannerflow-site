c0f7220962bae6ee23a1b154bbba5e27125ea0cd
Jakkarin-ko <jakkarin2534@gmail.com>
2025-10-28 06:24:22 +0700

    Update ServerStatusManager.tsx

--- src/ServerStatusManager.tsx
+++ src/ServerStatusManager.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import HeroCarousel from "./components//HeroCarousel.tsx";
+import HeroCarousel from "./components/HeroCarousel.tsx";
 import { Loader2, Zap, AlertTriangle } from "lucide-react"; 
 
 // ⭐️ กำหนด API Endpoint ⭐️
