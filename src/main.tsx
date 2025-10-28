006a64ee20437c8e68e12fa244de3e775d67da76
Jakkarin-ko <jakkarin2534@gmail.com>
2025-10-28 13:20:31 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // ✅ เพิ่ม BrowserRouter
+import { HashRouter } from 'react-router-dom'; // ✅ เพิ่ม BrowserRouter
 import ServerStatusManager from './ServerStatusManager.tsx'; // ⭐️ นำเข้า Component ใหม่
 import './index.css';
 
